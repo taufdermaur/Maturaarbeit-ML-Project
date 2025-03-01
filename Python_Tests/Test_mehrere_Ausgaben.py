cc070687e574aeb3ed24b4dfd051ee254510290c
Tim Auf der Maur <email@timaufdermaur.ch>
2025-03-01 17:39:58 +0100

    Neurales Netz mit mehreren Ausgaben

---
+++ Python_Tests/Test_mehrere_Ausgaben.py
@@ -0,0 +1,20 @@
+weights = [0.3, 0.2, 0.9]
+
+wlrec = [0.65, 0.8, 0.8, 0.9]
+
+input = wlrec[0]
+
+
+def ele_mul(number, vector):
+    output = [0, 0, 0]
+    assert(len(output) == len(vector))
+    for i in range(len(vector)):
+        output[i] = number * vector[i]
+    return output
+
+def neural_network(input, weights):
+    pred = ele_mul(input, weights)
+    return pred
+
+pred = neural_network(input, weights) #Vorhersagen (Zahlenvektor)
+print(pred)
