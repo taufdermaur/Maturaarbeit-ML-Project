65db906ee83cf0f51b8d887796fd1cc458858971
Tim Auf der Maur <email@timaufdermaur.ch>
2025-09-13 01:48:37 +0200

    CPU trainingszeit testen (Ohne CUDA)

--- Quellcode_MATA_Tim_AufderMaur/ASTRA_GTSRB_CNN.py
+++ Quellcode_MATA_Tim_AufderMaur/ASTRA_GTSRB_CNN.py
@@ -32,7 +32,7 @@ gpu_util_history = []
 timestamps = []
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda') #torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Device: {device}")
 
 # Data transformations
